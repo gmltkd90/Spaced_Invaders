71ea1b81f09e395b826ad2f7bd6ca8e21d48eba8
Hee Sang Yoo <gmltkd90@gmail.com>
2019-10-06 10:30:26 -0700

    Initial commit

---
+++ alien.py
@@ -0,0 +1,66 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """A class to represent a single alien in the fleet."""
+
+    def __init__(self, ai_settings, screen):
+        """Initialize the alien and set its starting position."""
+        super(Alien, self).__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # Load the alien image and set its rect attribute.
+        self.image = pygame.image.load('image/alien11.png')
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the alien's exact position.
+        self.x = float(self.rect.x)
+
+        self.alien_points = 50
+
+    def blitme(self):
+        """Draw the alien at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+    def check_edges(self):
+        """Return True if alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """Move the alien right or left."""
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
+        self.rect.x = self.x
+
+
+class Alien1(Alien):
+    def __init__(self, ai_settings, screen):
+        super().__init__(ai_settings, screen)
+        self.image = pygame.image.load('image/alien33.png')
+
+        self.ai_settings.alien1_points = 10
+
+
+class Alien2(Alien):
+    def __init__(self, ai_settings, screen):
+        super().__init__(ai_settings, screen)
+        self.image = pygame.image.load('image/alien22.png')
+
+        self.ai_settings.alien1_points = 20
+
+
+class Alien3(Alien):
+    def __init__(self, ai_settings, screen):
+        super().__init__(ai_settings, screen)
+        self.image = pygame.image.load('image/alien11.png')
+
+        self.ai_settings.alien1_points = 30
---
+++ settings.py
@@ -0,0 +1,51 @@
+class Settings():
+    """A Class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's static settings."""
+        # Screen settings
+        self.screen_width = 800
+        self.screen_height = 700
+        self.bg_color = (0, 0, 0)
+
+        self.ship_limit = 3
+
+        # Bullet settings
+        self.bullet_width = 4
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 5
+
+        # Alien settings
+        self.fleet_drop_speed = 2
+
+        # How quickly the game speeds up
+        self.speedup_sacle = 1.1
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize speed setting that change throughout the game."""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+        # Scoring
+        self.alien1_points = None
+        #self.alien2_points = 80
+        #self.alien3_points = 100
+
+    def increase_speed(self):
+        """Increase speed settings and alien point values."""
+        self.ship_speed_factor *= self.speedup_sacle
+        self.bullet_speed_factor *= self.speedup_sacle
+        self.alien_speed_factor *= self.speedup_sacle
+
+        self.alien1_points = int(self.alien1_points * self.score_scale)
+        print(self.alien_points)
+
